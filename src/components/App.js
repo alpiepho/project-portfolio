4d4beb980c1e03d9fcfc447e657f6faa54e16e17
al <alpiepho@yahoo.com>
2019-05-15 21:14:22 -0600

    several changes to improve Lighthouse audit and support PWA

--- src/components/App.js
+++ src/components/App.js
@@ -20,47 +20,47 @@ class App extends Component {
       {
         text: "RPN Hex Calculator Chrome Extension (Published to Chrome Store)",
         href: "https://chrome.google.com/webstore/detail/rpn-hex-calculator/koffmhlebmnnlgdlgfkpiglpfgjlkcmc",
-        image: "images/screenshot-rpn-hex-chrome.png?raw=true"
+        image: "images/screenshot-rpn-hex-chrome.png"
       },
       {
         text: "RPN Hex Calculator VS Code Extension (Published to Microsoft Marketplace)",
         href: "https://marketplace.visualstudio.com/items?itemName=thatnamegroup.rpn-hex-calc",
-        image: "images/screenshot-rpn-hex-vscode.png?raw=true"
+        image: "images/screenshot-rpn-hex-vscode.png"
       },
       {
         text: "Started Daily CSS Challenge to create complex graphics with just HTML and CSS. Have completed all 50. So amazing what is possible with just CSS!",
         href: "https://codepen.io/collection/DYpQdK/",
-        image: "images/screenshot-purecss.png?raw=true"
+        image: "images/screenshot-purecss.png"
       },
       {
         text: "With any luck this is not another boring old JavaScript calculator. This one has a cool font (if I say so myself) and lets you pick the animation for the button clicks. Enjoy!",
         href: "https://codepen.io/sd3x/full/qmMGqZ/",
-        image: "images/screenshot-js-calculator.png?raw=true"
+        image: "images/screenshot-js-calculator.png"
       },
       {
         text: "Finished the Free Code Camp Data Visualization Certification (before the refactor of the site)",
         href: "", // link is bad "https://www.freecodecamp.org/apiepho/data-visualization-certification"
-        image: "images/screenshot-fcc-dv-cert.png?raw=true"
+        image: "images/screenshot-fcc-dv-cert.png"
       },
       // {
       //   text: "A freeCodeCamp Data Visuallization project to implement a Rouge-like game. Written in React.js. Seemed like a simple project, but was very involved.",
       //   href: "https://codepen.io/sd3x/full/ayQMLb/",
-      //   image: "images/screenshot-roguelike.png?raw=true"
+      //   image: "images/screenshot-roguelike.png"
       // },
       {
         text: "Finished the Free Code Camp Front Development Certification!! Only complaint is the number of cups of coffee that went cold because I was so focused :)",
         href: "https://www.freecodecamp.com/apiepho/front-end-certification",
-        image: "images/screenshot-fcc-fe-cert.png?raw=true"
+        image: "images/screenshot-fcc-fe-cert.png"
       },
       {
         text: "This is a game modeled after the original Simon game. See the link at the bottom for the original game instructions.",
         href: "https://codepen.io/sd3x/full/mmobeZ/",
-        image: "images/screenshot-simon.png?raw=true"
+        image: "images/screenshot-simon.png"
       },
       {
         text: "A freeCodeCamp project, Tic Tac Toe game with many options. 1 or 2player.",
         href: "https://github.com/alpiepho/project-tic-tac-toe",
-        image: "images/screenshot-tic-tac-toe.png?raw=true"
+        image: "images/screenshot-tic-tac-toe.png"
       }
     ];
   
@@ -134,7 +134,7 @@ class App extends Component {
         title: "Commandline Heros",
         text: "An original podcast about the people who transform technology from the command line up.",
         href: "https://www.redhat.com/en/command-line-heroes",
-        image: "images/screenshot-commandline-heros.png?raw=true"
+        image: "images/screenshot-commandline-heros.png"
       },
       {
         title: "Intellegence Squared",

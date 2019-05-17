f4f883fc7bd1d18f50b2d702e8db72a9412ac9ab
Al Piepho <alpiepho@yahoo.com>
2019-05-16 21:51:02 -0600

    Updates

---
+++ precache-manifest.458073e08adf3f2fa2362628d27a7a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81667a6b183405c675ae6b49b6503e91",
+    "url": "/project-portfolio/index.html"
+  },
+  {
+    "revision": "de76ccae4b7f749ef0b2",
+    "url": "/project-portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1b75c5da0b1ef4d82f78",
+    "url": "/project-portfolio/static/css/main.f1222115.chunk.css"
+  },
+  {
+    "revision": "de76ccae4b7f749ef0b2",
+    "url": "/project-portfolio/static/js/2.98917bee.chunk.js"
+  },
+  {
+    "revision": "1b75c5da0b1ef4d82f78",
+    "url": "/project-portfolio/static/js/main.bf9e9a8c.chunk.js"
+  },
+  {
+    "revision": "8f56f2ad4f2f5b37fcee",
+    "url": "/project-portfolio/static/js/runtime~main.cf7ddae3.js"
+  }
+]);
